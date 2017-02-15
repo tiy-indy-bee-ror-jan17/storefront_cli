e167132a9407c5a4c41046816d089c72e1ae3b1c
Nancy Ingels <ingelsna@gmail.com>
2017-02-15 15:13:11 -0500

    created user table

--- storefront.rb
+++ storefront.rb
@@ -1,13 +1,26 @@
+
+require 'pry'
 require 'rubygems'
 require 'bundler/setup'
 require 'tty'
 require 'active_record'
 require 'sqlite3'
-require_relative 'password'
+require_relative 'models/user'
+
+ActiveRecord::Base.logger = Logger.new(STDOUT)
 
 ActiveRecord::Base.establish_connection(
   :adapter => 'sqlite3',
   :database => 'db/store.sqlite3'
 )
 
+
+#binding.pry
 # Your code here
+
+
+User.all.each do |x|
+  puts "Users: #{x.id} #{x.first_name} #{x.last_name} #{x.email}"
+end
+
+puts "The number of users in the table is #{User.count}"
